d3995bcf8abdd9e228cafe298908bf3da22d8ba2
ArthurMattenhauerSAYMYNAME <arthurzera.07@gmail.com>
2026-09-24 16:42:29 -0300

    imagem

--- laptops/add.php
+++ laptops/add.php
@@ -5,41 +5,68 @@
 
 <h2 class="mt-3">Adicionar Laptop</h2>
 
-<form action="add.php" method="post">
-  <!-- area de campos do form -->
+<form action="add.php" method="post" enctype="multipart/form-data">
   <hr />
-  <div class="row">
-    <div class="form-group col-md-6">
-      <label for="marca">Marca</label>
-      <input type="text" class="form-control" id="marca" name="laptop['marca']">
-    </div>
-    </div>
-   <div class="row">
-
-    <div class="form-group col-md-6">
-      <label for="modelo">Modelo</label>
-      <input type="text" class="form-control" id="modelo" name="laptop['modelo']" maxlength="15">
-    </div>
-  </div>
   
   <div class="row">
-    <div class="form-group col-md-3">
-      <label for="tamanho">Tamanho</label>
-      <input type="text" class="form-control" id="tamanho" name="laptop['tamanho']">
+    <div class="col-md-6">
+      <div class="form-group mb-3">
+        <label for="marca">Marca</label>
+        <input type="text" class="form-control" id="marca" name="laptop['marca']">
+      </div>
+
+      <div class="form-group mb-3">
+        <label for="modelo">Modelo</label>
+        <input type="text" class="form-control" id="modelo" name="laptop['modelo']" maxlength="15">
+      </div>
+
+      <div class="row">
+        <div class="form-group col-md-6 mb-3">
+          <label for="tamanho">Tamanho</label>
+          <input type="text" class="form-control" id="tamanho" name="laptop['tamanho']">
+        </div>
+
+        <div class="form-group col-md-6 mb-3">
+          <label for="datacad">Data de Cadastro</label>
+          <input type="date" class="form-control" id="datacad" name="laptop['datacad']" disabled>
+        </div>
+      </div>
+
+      <div class="form-group mb-3">
+        <label for="foto">Foto do Laptop</label>
+        <input type="file" class="form-control" id="foto" name="foto" accept="image/*" onchange="previewImagem(event)">
+      </div>
     </div>
 
-    <div class="form-group col-md-3">
-      <label for="datacad">Data de Cadastro</label>
-      <input type="date" class="form-control" id="datacad"  name="laptop['datacad']" disabled>
+    <div class="col-md-6 d-flex align-items-center justify-content-center">
+      <img id="img-preview" src="<?php echo BASEURL; ?>assets/img/default.png" alt="Preview da Imagem" class="foto-laptop-view" style="display: none;">
     </div>
   </div>
-  
-  <div id="actions" class="row">
+
+  <div id="actions" class="row mt-3">
     <div class="col-md-12">
-      <button type="submit" class="btn btn-azul mt-3 text-offwhite"><i class="fa-solid fa-floppy-disk text-offwhite"></i>Salvar</button>
-      <a href="index.php" class="btn btn-azulclaro mt-3 text-offwhite"><i class="fa-solid fa-circle-left text-offwhite"></i>Cancelar</a>
+      <button type="submit" class="btn btn-azul text-offwhite"><i class="fa-solid fa-floppy-disk text-offwhite"></i> Salvar</button>
+      <a href="index.php" class="btn btn-azulclaro text-offwhite"><i class="fa-solid fa-circle-left text-offwhite"></i> Cancelar</a>
     </div>
   </div>
 </form>
+
+<script>
+  function previewImagem(event) {
+    const input = event.target;
+    const preview = document.getElementById('img-preview');
+
+    if (input.files && input.files[0]) {
+      const reader = new FileReader();
+
+      reader.onload = function(e) {
+        preview.src = e.target.result;
+        preview.style.display = 'block';
+      }
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+</script>
 
 <?php include(FOOTER_TEMPLATE); ?>
--- laptops/edit.php
+++ laptops/edit.php
@@ -6,41 +6,47 @@
 <h2 class="mt-3">Atualizar Laptop Cadastrado</h2>
 
 <form action="edit.php?id=<?php echo $laptop['id']; ?>" method="post"> 
-  <!-- area de campos do form -->
   <hr />
-  <div class="row">
-    <div class="form-group col-md-6">
-      <label for="marca">Marca</label>
-      <input type="text" class="form-control" id="marca" name="laptop['marca']" value="<?php echo $laptop['marca']; ?>">
-    </div>
-    </div>
-   <div class="row">
-
-    <div class="form-group col-md-6">
-      <label for="modelo">Modelo</label>
-      <input type="text" class="form-control" id="modelo" name="laptop['modelo']" maxlength="15" value="<?php echo $laptop['modelo']; ?>">
-    </div>
-  </div>
   
   <div class="row">
-    <div class="form-group col-md-3">
-      <label for="tamanho">Tamanho</label>
-      <input type="text" class="form-control" id="tamanho" name="laptop['tamanho']" value="<?php echo $laptop['tamanho']; ?>">
-    </div>
+    <
+    <div class="col-md-6">
+      <div class="form-group mb-3">
+        <label for="marca">Marca</label>
+        <input type="text" class="form-control" id="marca" name="laptop['marca']" value="<?php echo $laptop['marca']; ?>">
+      </div>
 
-    <div class="form-group col-md-3">
-      <label for="datacad">Data de Cadastro</label>
-      <input type="date" class="form-control" id="datacad"  name="laptop['datacad']" disabled value="<?php echo formatdata($laptop['datacad'], "d/m/Y - H:i:s"); ?>">
+      <div class="form-group mb-3">
+        <label for="modelo">Modelo</label>
+        <input type="text" class="form-control" id="modelo" name="laptop['modelo']" maxlength="15" value="<?php echo $laptop['modelo']; ?>">
+      </div>
+
+      <div class="row">
+        <div class="form-group col-md-6 mb-3">
+          <label for="tamanho">Tamanho</label>
+          <input type="text" class="form-control" id="tamanho" name="laptop['tamanho']" value="<?php echo $laptop['tamanho']; ?>">
+        </div>
+
+        <div class="form-group col-md-6 mb-3">
+          <label for="datacad">Data de Cadastro</label>
+          <input type="date" class="form-control" id="datacad" name="laptop['datacad']" disabled value="<?php echo formatdata($laptop['datacad'], "d/m/Y - H:i:s"); ?>">
+        </div>
+      </div>
     </div>
 
     
-    
+    <div class="col-md-6 d-flex align-items-center justify-content-center">
+      <img src="<?php echo BASEURL; ?>assets/img/<?php echo $laptop['foto']; ?>" 
+           alt="<?php echo $laptop['modelo']; ?>" 
+           class="foto-laptop-edit">
+    </div>
   </div>
-  
-  <div id="actions" class="row">
+
+  <!-- Ações (Botões) -->
+  <div id="actions" class="row mt-3">
     <div class="col-md-12">
-      <button type="submit" class="btn btn-azul mt-3 text-offwhite"><i class="fa-solid fa-floppy-disk text-offwhite"></i>Salvar</button>
-      <a href="index.php" class="btn btn-azulclaro mt-3 text-offwhite"><i class="fa-solid fa-circle-left text-offwhite"></i>Cancelar</a>
+      <button type="submit" class="btn btn-azul text-offwhite"><i class="fa-solid fa-floppy-disk text-offwhite"></i> Salvar</button>
+      <a href="index.php" class="btn btn-azulclaro text-offwhite"><i class="fa-solid fa-circle-left text-offwhite"></i> Cancelar</a>
     </div>
   </div>
 </form>
