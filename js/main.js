1c7016554add1e9fab7cd05ccb1dbbf403c16c26
DanDani-01 <daniel.rt.oliveira@gmail.com>
2026-09-24 23:39:50 -0300

    Finalizado

--- js/main.js
+++ js/main.js
@@ -5,6 +5,6 @@ $('#delete-modal').on('show.bs.modal', function (event) {
   
   var modal = $(this);
   modal.find('.modal-title').text('Excluir Laptop #' + id);
-  modal.find('.modal-body').text('Deseja Mesmo Excluir o Laptop #' + id + '? <br>Esta ação não poderá ser desfeita.');
+  modal.find('.modal-body').text('Deseja Mesmo Excluir o Laptop #' + id + '? Esta ação não poderá ser desfeita.');
   modal.find('#confirm').attr('href', 'delete.php?id=' + id);
 })
